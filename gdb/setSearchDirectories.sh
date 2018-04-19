7bfd628ce551eb6b58c710b4081c63e576b68de7
Wei Ning <nwcgnw@gmail.com>
2018-04-19 12:51:15 +1200

    - update gdb search path example

--- gdb/setSearchDirectories.sh
+++ gdb/setSearchDirectories.sh
@@ -5,19 +5,26 @@ function gdbDirArgs() {
 import os
 import sys
 rootDir = sys.argv[1]
-extensions = set(['.{}'.format(_) for _ in sys.argv[2:]])
+extensions = set()
+excludedDirs = set()
+for arg_ in sys.argv[2:]:
+    if arg_.startswith('-'):
+        excludedDirs.add(arg_[1:])
+    else:
+        extensions.add('.{}'.format(arg_)) 
 dirs = set()
 for curDir, subDirs, fileNames in os.walk(rootDir):
+    subDirs[:] = [d for d in subDirs if d not in excludedDirs]
     for fileName in fileNames:
         if os.path.splitext(fileName)[-1] not in extensions:
             continue
         dirs.add('-d {}'.format(curDir))
 print ' '.join(dirs)
-" ${@} 2>/dev/null
+" ${@}
 }
 
 function run() {
-    gdb $( gdbDirArgs /work/dev/c/github.com/powergun/cexamples/tests c h hpp py ) -batch -ex "show directories"
+    gdb $( gdbDirArgs /work/dev/c/github.com/powergun/cexamples c h -.git -build -cmake-build-debug ) -batch -ex "show directories"
 }
 
 run
