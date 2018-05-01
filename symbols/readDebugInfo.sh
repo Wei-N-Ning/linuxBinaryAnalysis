c94cdb2a316e94849ff99be8bd1514c0dd1963ba
Wei Ning <nwcgnw@gmail.com>
2018-05-02 07:36:08 +1200

    - update read debug info example (compare the result from readelf and objdump)

--- symbols/readDebugInfo.sh
+++ symbols/readDebugInfo.sh
@@ -3,13 +3,33 @@
 srcPath="../_sut/minimal.c"
 binPath="/tmp/$( basename ${srcPath} ).o"
 
-function compile() {
-    gcc -o ${binPath} -g ${srcPath}
+function assertZero() { if [ "${1}" != "0" ]; then echo "failed"; exit 1; fi }
+
+function assertNotZero() { if [ "${1}" == "0" ]; then echo "failed"; exit 1; fi }
+
+function buildSUT() {
+    gcc -o ${binPath} ${1} ${srcPath}
 }
 
-function readDebugInfo() {
-    objdump --debugging ${binPath}
+function verify_objdump() {
+    objdump --debugging ${binPath} 2>/dev/null | wc -l
 }
 
-compile
-readDebugInfo
+function verify_readelf() {
+    readelf -p .debug_info ${binPath} 2>/dev/null | wc -l
+}
+
+function demo_withDebugInfo() {
+    buildSUT "-g"
+    verify_objdump
+    assertNotZero $( verify_readelf )
+}
+
+function demo_withoutDebugInfo() {
+    buildSUT
+    verify_objdump
+    assertZero $( verify_readelf )
+}
+
+demo_withDebugInfo
+demo_withoutDebugInfo
