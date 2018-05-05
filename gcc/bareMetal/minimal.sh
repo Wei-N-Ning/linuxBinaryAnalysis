80409a11d0cd5d5fc93e1d16452bbdc321f20358
Wei Ning <nwcgnw@gmail.com>
2018-05-05 23:12:48 +1200

    - add new content for gcc sub project: how to build a bare metal c program without stdlib (-nostdlib)

---
+++ gcc/bareMetal/minimal.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/env bash
+
+function buildSUT {
+    sut=/tmp/_.c
+    out=/tmp/_
+    rm -f ${sut} ${out}
+    echo '
+void _start() {
+    __asm__("movl $1, %eax");
+    __asm__("xorl %ebx, %ebx");
+    __asm__("int $0x80");
+}
+' > ${sut}
+}
+
+function compileGCC() {
+    gcc -nostdlib -o ${out} ${sut}
+}
+
+function compileCLANG() {
+    clang -nostdlib -o ${out} ${sut}
+}
+
+function test() {
+    ${out}
+}
+
+function run() {
+    buildSUT
+    compileGCC
+    test
+    compileCLANG
+    test
+}
+
+run
