9f718fbd2a179ba2b9f3118dc95d99765381d6f6
Wei Ning <wning@wetafx.co.nz>
2018-06-21 16:30:32 +1200

    - tcc prototype

--- gcc/tcc.sh
+++ gcc/tcc.sh
@@ -5,26 +5,32 @@
 # dotnet run $FILE.cs
 # to automate the building and running of a single c program
 
-CC=
-SUT=
+WHICH=/bin/which
+CC=cc
+CXX=c++
+SUT=/tmp/sut
+OUT_FILE=${SUT}/_
+FLAGS=
+CC_FLAGS=
+CXX_FLAGS=
+
+APP=
+APP_ARGS=
 
 setUp() {
-    set -e
-    ls /bin/which >/dev/null 2>&1
-    which cc >/dev/null 2>&1
-    CC=cc
-    SUT=/tmp/sut
+    ls ${WHICH} >/dev/null 2>&1
+    ${WHICH} ${CC} >/dev/null 2>&1
+    ${WHICH} ${CXX} >/dev/null 2>&1
     rm -rf ${SUT}
     mkdir -p ${SUT}
 }
 
-# $1: source file
 buildProgram() {
-    ${CC} $1 -o ${SUT}/_
+    ${CC} $1 -o ${OUT_FILE}
 }
 
 runProgram() {
-    ${SUT}/_
+    ${OUT_FILE}
 }
 
 tearDown() {
@@ -32,8 +38,45 @@ tearDown() {
     echo "DONE"
 }
 
+parseArgs() {
+    if [[ "${1}" != "--" ]]; then
+        APP=${1}
+        shift 1
+    fi
+    while [[ ${#} -gt 0 ]]; do
+        if [[ "${1}" == "--" ]]; then
+            break
+        fi
+        APP_ARGS="${APP_ARGS} ${1}"
+        shift 1
+    done
+    if [[ ${#} -gt 0 ]]; then
+        shift
+        FLAGS=${@}
+    fi
+}
+
+validate() {
+    if [[ "${APP}" == "" ]]; then
+        echo "missing app name"
+        exit 1
+    fi
+}
+
+run_app() {
+    if [[ "${APP}" == "run" ]]; then
+        buildProgram ${APP_ARGS}
+        runProgram ${APP_ARGS}
+        return 0
+    else
+        echo "unsupported app: ${APP}"
+        exit 1
+    fi
+}
+
 setUp
-buildProgram $1
-runProgram
+parseArgs $@
+validate
+run_app
 tearDown
 
