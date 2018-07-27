151d44737b21b96717c7ec3d7cadaf325ac311b7
Wei Ning <nwcgnw@gmail.com>
2018-07-27 23:30:43 +1200

    - moved out gdb

--- gdb/breakpoints/plt.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-# what does @plt mean?
-# more often than not, while searching for the subroutine symbols,
-# some fake dummies show up such as printf()@plt
-# this post explains it:
-# https://stackoverflow.com/questions/5469274/what-does-plt-mean-here
-
-
