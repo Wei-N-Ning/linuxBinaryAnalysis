1c5c1c46ca5bd702cf37e408d1fa141306c490f2
Wei Ning <nwcgnw@gmail.com>
2018-07-27 14:19:00 +1200

    - add basic tracing example (and it is not too slow)

--- gdb/tracing/tracepoints.sh
+++ gdb/tracing/tracepoints.sh
@@ -10,7 +10,8 @@
 # time, after which the program gets to continue with minimal disruption
 # of its behavior.
 
-# During this "short" interval, the trace mechanism records the fact that it has been there (ie. that the tracepoint
+# During this "short" interval, the trace mechanism records the fact
+# that it has been there (ie. that the tracepoint
 # was executed), and may also perform certain actions that you've
 # requested such as recording the values of selected variables and
 # registers. Thus it's not well defined how long a "short" time is,
@@ -19,6 +20,27 @@
 
 # I'm using the same SUT program inspired by some drunk chicken code
 # from wt
+
+# NOTE:
+# see this first:
+# https://stackoverflow.com/questions/3691394/gdb-meaning-of-tstart-error-you-cant-do-that-when-your-target-is-exec
+# I'm quite close but stuck at understanding the data collection part:
+# https://sourceware.org/gdb/talks/esc-west-1999/commands.html
+# following the first link I was able to collect some trace record but
+# no data
+
+# See this: https://stackoverflow.com/questions/311948/make-gdb-print-control-flow-of-functions-as-they-are-called
+# this one might be what I really want
+
+# this is what I need:
+# define functiontrace
+#if $arg0
+#    break $arg0
+#    commands
+#        where
+#        continue
+#        end
+#    end
 
 set -e
 
@@ -47,3 +69,4 @@ buildProgram() {
 
 setUp
 buildProgram
+
