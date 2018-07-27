12d70434e23ec653a385d2642db2f63d2cf27033
Wei Ning <wning@wetafx.co.nz>
2018-07-27 17:19:52 +1200

    - add explanation to @plt symbol

---
+++ gdb/breakpoints/plt.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+# what does @plt mean?
+# more often than not, while searching for the subroutine symbols,
+# some fake dummies show up such as printf()@plt
+# this post explains it:
+# https://stackoverflow.com/questions/5469274/what-does-plt-mean-here
+
+
