cffe63ef60f6cce38f0f989a86e4363717741ca5
Wei Ning <nwcgnw@gmail.com>
2018-08-29 19:31:46 +1200

    - example of figuring out heap and stack region

---
+++ memory/elf_heap_stack.sh
@@ -0,0 +1,12 @@
+#/usr/bin/env bash
+
+# source:
+# https://www.linuxjournal.com/article/4681
+# see also:
+# https://support.cloudbees.com/hc/en-us/articles/222167128-How-to-generate-a-heap-dump-
+
+# do:
+# cat /proc/xxxx/maps | grep -i stack
+# cat /proc/xxxx/maps | grep -i heap
+
+
